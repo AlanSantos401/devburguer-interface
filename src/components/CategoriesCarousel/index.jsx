60518034f3b5f8768b1d0ac30951a48a3782b0ca
Alan <alandsantos54@gmail.com>
2025-02-27 14:27:42 -0300

    DevBurger - Interface pt.1 - Aula 24

--- src/components/CategoriesCarousel/index.jsx
+++ src/components/CategoriesCarousel/index.jsx
@@ -1,6 +1,10 @@
 import { useState } from 'react';
-import { api } from '../../services/api';
 import { useEffect } from 'react';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
+import { api } from '../../services/api';
+import { Container, Title, ContainerItems } from './styles';
 
 export function CategoriesCarousel(){
     const [categories, setCategories] = useState([]);
@@ -8,16 +12,50 @@ export function CategoriesCarousel(){
     useEffect(() => {
        
         async function loadCategories() {
-            const res = await api.get('/categories');
-            console.log(res)
+            const { data } = await api.get('/categories');
+            
+            setCategories(data);
+            console.log(data);
         }
         
         loadCategories();
     }, []);
 
+    const responsive = {
+        superLargeDesktop: {
+            breakpoint: {max: 4000, min: 3000},
+            items: 4,
+        },
+        desktop: {
+            breakpoint: {max: 3000, min: 1280},
+            items: 4,
+        },
+        tablet: {
+            breakpoint: {max: 1280, min: 690},
+            items: 3,
+        },
+        mobile: {
+            breakpoint: {max: 690, min: 0},
+            items: 2,
+        },
+    };
+
     return (
-        <div>
-            <h1>ok</h1>
-        </div>
+        <Container>
+            <Title>Categorias</Title>
+
+            <Carousel 
+              responsive={responsive}
+              infinite={true}
+              partialVisible={false}
+              itemClass='carousel-item'
+            >
+                {categories.map( category => (
+                   <ContainerItems key={category.id} imageUrl={category.url}>
+                    <p>{category.name}</p></ContainerItems>
+                ))}
+                
+            </Carousel>
+        </Container>
     );
 }
